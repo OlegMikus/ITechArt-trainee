4f7432521a0abeb98c7d670731f5e6987d3b960d
OlegMikus <aleh.mikus@itechart-group.com>
2021-09-09 18:40:11 +0300

    add: login requared decorator

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+
 from . import db
 
 main = Blueprint('main', __name__)
@@ -9,8 +11,7 @@ def index():  # put application's code here
     return render_template('index.html')
 
 
+@login_required
 @main.route('/profile')
 def profile():
-    return render_template('profile.html')
-
-
+    return render_template('profile.html', name=current_user.name)
