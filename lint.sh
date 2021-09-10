02c4f4e329bcc18275c7f6a84faf211ed0391eb3
OlegMikus <aleh.mikus@itechart-group.com>
2021-09-10 16:06:30 +0300

    add linter

---
+++ lint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo ">>> $(basename ${BASH_SOURCE[0]})"
+
+set -o errexit # exit script when command fails
+set -o pipefail # this setting prevents errors in a pipeline from being masked
+set -o nounset # exit script when it tries to use undeclared variables
+
+echo 'pylint'
+pylint --load-plugins pylint_flask .
+echo 'flake8'
+flake8 .
+echo 'pycodestyle'
+pycodestyle --first .
+echo 'mypy'
+mypy . --exclude migrations
+
+echo ">>> $(basename ${BASH_SOURCE[0]}) DONE"
