97ff3eae78d499a1d8853b01678dc6211d1be908
OlegMikus <aleh.mikus@itechart-group.com>
2021-09-17 15:27:33 +0300

    add jwt authentication support

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,20 +1,42 @@
-from flask import request, jsonify, Response
+from datetime import datetime, timedelta
+from functools import wraps
+from typing import Any
+
+import jwt
+from flask import request, jsonify, Response, session
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from src.models.user import User
 from src.config.app import db, app
 
 
+def token_required(func: Any) -> Any:
+    @wraps(func)
+    def decorated(*args: Any, **kwargs: Any) -> Response:
+        token = request.headers['token']
+        if not token:
+            return jsonify({'Alert!': 'Token is missing!'})
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except jwt.InvalidTokenError:
+            return jsonify({'Message': 'Invalid token. Please log in again.'})
+        return func(data, *args, **kwargs)
+
+    return decorated
+
+
 @app.route('/login', methods=['POST'])
 def login() -> Response:
     request_data = request.get_json()
-    email = request_data['email']
-    password = str(request_data['password'])
-    user = User.query.filter_by(email=email).first()
-    if not user or not check_password_hash(user.password, password):
-        return jsonify({'info': 'smth wrong'})
+    user = User.query.filter_by(email=request_data['email']).first()
+    if request_data['email'] and check_password_hash(user.password, str(request_data['password'])):
+        token = jwt.encode({'user_email': request_data['email'],
+                            'expiration': str(datetime.utcnow() + timedelta(seconds=60))},
+                           app.config['SECRET_KEY'])
 
-    return jsonify({'info': 'you are log IN'})
+        return jsonify({'token': token.decode('UTF-8')})
+
+    return jsonify({'info': 'something wrong'})
 
 
 @app.route('/signup', methods=['POST'])
@@ -36,15 +58,17 @@ def signup() -> Response:
 
 
 @app.route('/logout')
+@token_required
 def logout() -> Response:
-    return jsonify({'info': 'you are log OUT'})
+    ''' This function doesn't work '''
+    if session['logged_in']:
+        session['logged_in'] = False
+        return jsonify({'message': 'You successfully logged out'})
+    return jsonify({'message': 'error'})
 
 
 @app.route('/profile')
-def profile() -> str:
-    return 'str'
-
-
-@app.route('/')
-def index() -> Response:
-    return jsonify({'page': 'main page'})
+@token_required
+def profile() -> Response:
+    request_data = request.get_json()
+    return jsonify({'info': request_data, })
