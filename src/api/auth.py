8bcb8b6df9f2cf445a928ab423cccd785510c434
OlegMikus <aleh.mikus@itechart-group.com>
2021-09-17 18:52:19 +0300

    add refresh endpoint, modify login endpoint

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,28 +1,9 @@
-from datetime import datetime, timedelta
-from functools import wraps
-from typing import Any
-
-import jwt
 from flask import request, jsonify, Response, session
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from src.services.tokens import token_required, create_access_token, create_refresh_token, refresh_required
 from src.models.user import User
 from src.config.app import db, app
-
-
-def token_required(func: Any) -> Any:
-    @wraps(func)
-    def decorated(*args: Any, **kwargs: Any) -> Response:
-        token = request.headers['token']
-        if not token:
-            return jsonify({'Alert!': 'Token is missing!'})
-        try:
-            data = jwt.decode(token, app.config['SECRET_KEY'])
-        except jwt.InvalidTokenError:
-            return jsonify({'Message': 'Invalid token. Please log in again.'})
-        return func(data, *args, **kwargs)
-
-    return decorated
 
 
 @app.route('/login', methods=['POST'])
@@ -30,13 +11,27 @@ def login() -> Response:
     request_data = request.get_json()
     user = User.query.filter_by(email=request_data['email']).first()
     if request_data['email'] and check_password_hash(user.password, str(request_data['password'])):
-        token = jwt.encode({'user_email': request_data['email'],
-                            'expiration': str(datetime.utcnow() + timedelta(seconds=60))},
-                           app.config['SECRET_KEY'])
+        session['logged_in'] = True
 
-        return jsonify({'token': token.decode('UTF-8')})
+        access_token = create_access_token(request_data)
+        refresh_token = create_refresh_token(request_data)
+
+        return jsonify({'access_token': access_token.decode('UTF-8'),
+                        'refresh_token': refresh_token.decode('UTF-8')})
 
     return jsonify({'info': 'something wrong'})
+
+
+@app.route('/refresh')
+@refresh_required
+def refresh_token() -> Response:
+
+    request_data = request.get_json()
+    access_token = create_access_token(request_data)
+    refresh_token = create_refresh_token(request_data)
+
+    return jsonify({'access_token': access_token.decode('UTF-8'),
+                    'refresh_token': refresh_token.decode('UTF-8')})
 
 
 @app.route('/signup', methods=['POST'])
@@ -57,18 +52,7 @@ def signup() -> Response:
     return jsonify({'info': 'congratulation'})
 
 
-@app.route('/logout')
-@token_required
-def logout() -> Response:
-    ''' This function doesn't work '''
-    if session['logged_in']:
-        session['logged_in'] = False
-        return jsonify({'message': 'You successfully logged out'})
-    return jsonify({'message': 'error'})
-
-
 @app.route('/profile')
 @token_required
 def profile() -> Response:
-    request_data = request.get_json()
-    return jsonify({'info': request_data, })
+    return jsonify({'info': 'If you see this, then you logged in'})
