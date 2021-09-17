65d5673ef74e7fd9228f4683e1cb8d77836e512c
OlegMikus <aleh.mikus@itechart-group.com>
2021-09-17 18:53:54 +0300

    add file with side functions

---
+++ src/services/tokens.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timedelta
+from functools import wraps
+from typing import Any
+
+import jwt
+from flask import request, jsonify, Response
+
+from src.config.app import app
+
+
+def token_required(func: Any) -> Any:
+    @wraps(func)
+    def decorated(*args: Any, **kwargs: Any) -> Response:
+        token = request.headers['token']
+        if not token:
+            return jsonify({'Alert!': 'Token is missing!'})
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            print(data)
+        except jwt.InvalidTokenError:
+            if jwt.InvalidTokenError == 'Signature has expired':
+                return jsonify({'Message': 'http://localhost:8000/refresh'})
+            return jsonify({'Message': 'Invalid token. Please log in again.'})
+        return func(*args, **kwargs)
+
+    return decorated
+
+
+def refresh_required(func: Any) -> Any:
+    @wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> Response:
+        token = request.headers['refresh_token']
+        if not token:
+            return jsonify({'Alert!': ' refresh Token is missing!'})
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            print(data)
+        except jwt.InvalidTokenError:
+            return jsonify({'Message': 'Invalid refresh token. Please log in again.'})
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def create_access_token(data: Any) -> bytes:
+    token = jwt.encode({
+        'user_email': data['email'],
+        'exp': datetime.utcnow() + timedelta(seconds=180),
+    },
+        app.config['SECRET_KEY'])
+    return token
+
+
+def create_refresh_token(data: Any) -> bytes:
+    token = jwt.encode({
+        'user_email': data['email'],
+        'exp': datetime.utcnow() + timedelta(days=30),
+    },
+        app.config['SECRET_KEY'])
+    return token
